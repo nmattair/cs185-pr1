c726128211ec6b5fdf2c767272a069ca158501dd
Nick Mattair <nick.mattair@ayokasystems.com>
2021-01-25 19:32:49 -0800

    Added email tab and scroll button

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const textLink= document.querySelector("#textLink");
 const imageLink= document.querySelector("#imageLink");
 const videoLink= document.querySelector("#videoLink");
 const tableLink= document.querySelector("#tableLink");
+const emailLink= document.querySelector("#emailLink")
 
 textLink.addEventListener("mouseover", function() {
     textLink.style.textDecoration = "underline";
@@ -39,6 +40,15 @@ if(document.title != "Table"){
     });
 }
 
+emailLink.addEventListener("mouseover", function() {
+    emailLink.style.textDecoration = "underline";
+});
+if(document.title != "Email"){
+    emailLink.addEventListener("mouseout", function() {
+        emailLink.style.textDecoration = "none";
+    });
+}
+
 if(document.title == "Text") {
     textLink.style.textDecoration = "underline";
 }
@@ -53,4 +63,35 @@ if(document.title == "Video") {
 
 if(document.title == "Table") {
     tableLink.style.textDecoration = "underline";
+}
+
+if(document.title == "Email") {
+    emailLink.style.textDecoration = "underline";
+}
+
+function validate() {
+    var email = document.getElementById("address").value;
+    if(email.includes("@") && (email.endsWith(".com") || email.endsWith(".edu"))){
+        document.querySelector("#response").innerHTML = "Email successfully recorded";
+    }
+    else{
+        document.querySelector("#response").innerHTML = "Invalid email address";
+    }
+}
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
